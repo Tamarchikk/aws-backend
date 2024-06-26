a225da7ec460b6f7f912d0754c8aa2860eeb92bf
Tamar <Tamar_Ratiani@epam.com>
2024-06-26 21:31:19 +0400

    changes for task-4

--- bin/aws-backend.js
+++ bin/aws-backend.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
 const cdk = require('aws-cdk-lib');
-const { AwsBackendStack } = require('../lib/aws-backend-stack');
+const { ProductService } = require('../lib/index');
 
 const app = new cdk.App();
-new AwsBackendStack(app, 'AwsBackendStack', {
+new ProductService(app, 'ProductService', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
